1ea9725dabf586bed30b0196a3ec9aa6456d91e2
RaeanneZ <raeannezou@gmail.com>
2024-05-04 22:46:40 +0800

    Finished Practical 3

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/ListActivity.java
@@ -0,0 +1,67 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Random random = new Random();
+        ImageView triggerImg = findViewById(R.id.Image);
+
+        triggerImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
+                builder.setTitle("Profile");
+                builder.setMessage("MADness");
+
+                builder.setCancelable(false);
+
+                builder.setPositiveButton("View", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    String randomNumber = Integer.toString(random.nextInt(999999999));
+
+                    // Launch MainActivity Page
+                    Intent intent = new Intent (ListActivity.this, MainActivity.class);
+                    intent.putExtra("randomNumber", randomNumber);
+                    startActivity(intent);
+                });
+
+                builder.setNegativeButton("Close", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+
+                // Create the Alert dialog
+                AlertDialog alertDialog = builder.create();
+                // Show the Alert Dialog box
+                alertDialog.show();
+            }
+
+        });
+    }
+}
